2f4060247a7b83b2d93f0cc72325cf61ef110474
sedef bas <bassedef98@gmail.com>
2024-11-01 17:17:57 +0300

    second commit

--- src/main/java/management/sedef/SedefApplication.java
+++ src/main/java/management/sedef/SedefApplication.java
@@ -7,7 +7,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SedefApplication {
 
 	public static void main(String[] args) {
+		System.out.printf("Welcome to SedefApplication!");
 		SpringApplication.run(SedefApplication.class, args);
 	}
+
 
 }
