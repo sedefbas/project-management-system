a2c928ad53367219576499643d606dc9f032746e
sedef bas <bassedef98@gmail.com>
2024-12-14 00:20:09 +0300

    added CompanyUser

---
+++ src/main/java/management/sedef/company/model/entity/CompanyUserEntity.java
@@ -0,0 +1,38 @@
+package management.sedef.company.model.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import management.sedef.user.model.entity.UserEntity;
+
+import java.time.LocalDate;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "company_user")
+public class CompanyUserEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "company_id", nullable = false)
+    private CompanyEntity company;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private UserEntity user;
+
+    @Column(name = "role")
+    private String role;
+
+    @Column(name = "start_date")
+    private LocalDate startDate;
+}
+
