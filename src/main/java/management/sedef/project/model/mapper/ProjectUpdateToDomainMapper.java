ba65309793ff6b555fbdd377aabfe23e13ff7c8e
sedef bas <bassedef98@gmail.com>
2025-01-30 16:06:37 +0300

    projectUpdate

---
+++ src/main/java/management/sedef/project/model/mapper/ProjectUpdateToDomainMapper.java
@@ -0,0 +1,17 @@
+package management.sedef.project.model.mapper;
+
+
+import management.sedef.common.model.entity.mapper.BaseMapper;
+import management.sedef.project.model.Project;
+import management.sedef.project.model.request.ProjectUpdateRequest;
+import org.mapstruct.Mapper;
+import org.mapstruct.factory.Mappers;
+
+@Mapper(componentModel = "spring")
+public interface ProjectUpdateToDomainMapper  extends BaseMapper<ProjectUpdateRequest, Project> {
+
+    static ProjectUpdateToDomainMapper initialize(){
+        return Mappers.getMapper(ProjectUpdateToDomainMapper.class);
+    }
+
+}
---
+++ src/main/java/management/sedef/project/model/request/ProjectUpdateRequest.java
@@ -0,0 +1,38 @@
+package management.sedef.project.model.request;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import lombok.*;
+import management.sedef.project.model.enums.ProjectStatus;
+
+import java.time.LocalDate;
+import java.util.List;
+
+
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class ProjectUpdateRequest {
+
+    @NotNull
+    private String name;
+
+    private String description;
+
+    private String photo;
+
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd")
+    private LocalDate startDate;
+
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd")
+    private LocalDate endDate;
+
+    private ProjectStatus status;
+
+    @NotEmpty(message = "Group IDs cannot be empty")
+    @NotNull(message = "Group IDs cannot be null")
+    private List<Long> groupIds;
+}
