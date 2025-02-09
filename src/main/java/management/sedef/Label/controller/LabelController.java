58f96ea88a9a755bffb08a161a35b253f1ac03bc
sedef bas <bassedef98@gmail.com>
2025-02-09 17:57:19 +0300

    FEAT : Add controllers for Label

---
+++ src/main/java/management/sedef/Label/controller/LabelController.java
@@ -0,0 +1,44 @@
+package management.sedef.Label.controller;
+
+
+import lombok.RequiredArgsConstructor;
+import management.sedef.Label.model.Label;
+import management.sedef.Label.model.request.LabelRequest;
+import management.sedef.Label.service.LabelService;
+import management.sedef.common.model.entity.response.SuccessResponse;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(path = "/api/v1/company/{companyId}/labels")
+@RequiredArgsConstructor
+public class LabelController {
+
+    private final LabelService labelService;
+
+
+    @PostMapping()
+    @PreAuthorize("hasAnyAuthority('label:create')")
+    public SuccessResponse<Void> save(@RequestBody LabelRequest request){
+        labelService.save(request);
+        return SuccessResponse.success();
+    }
+
+
+    @GetMapping
+    @PreAuthorize("hasAnyAuthority('label:detail')")
+    public SuccessResponse<List<Label>> findLabelsByCompanyId(@PathVariable Long companyId) {
+        List<Label> labels = labelService.findLabelsByCompanyId(companyId);
+        return SuccessResponse.success(labels);
+    }
+
+    @DeleteMapping("/{labelId}")
+    @PreAuthorize("hasAnyAuthority('label:delete')")
+    public SuccessResponse<Void> delete(@PathVariable Long labelId) {
+        labelService.delete(labelId);
+        return SuccessResponse.success();
+    }
+
+}
